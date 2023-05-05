f3cf984a0826604f637a05809a6012b1784e8ca1
Dagrab <91456538+DanielKostacevic@users.noreply.github.com>
2023-05-05 09:06:37 +0200

    feat: initial commit

--- app/src/main/java/com/example/a1v1minigame/MainActivity.java
+++ app/src/main/java/com/example/a1v1minigame/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.a1v1minigame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    int x = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void player1(View v){
+        Button button = findViewById(R.id.btn_p1);
+        button.setScaleY(button.getScaleY() + 1);
+        x++;
     }
 }
